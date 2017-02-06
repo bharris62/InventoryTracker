78760c90daa835db3cca7bd40e55393ccc28e8cd
Andrew Harris <blakebharris@gmail.com>
2017-02-06 15:04:43 -0600

    refactor into methods

--- src/Inventory.java
+++ src/Inventory.java
@@ -11,9 +11,6 @@ public class Inventory {
 
     public Inventory(){}
 
-    public int getQuant() {
-        return this.quantity;
-    }
 
     public String getItem(Scanner scanner){
         System.out.print("What would you like to add: ");
@@ -27,6 +24,36 @@ public class Inventory {
         return quantity;
     }
 
+    public void updateGarage(Scanner scanner){
+        System.out.println("What number would you like to update? ");
+        int idx = Integer.parseInt(scanner.nextLine());
+        System.out.println("What would you like to change to: ");
+        int toUpdate = Integer.parseInt(scanner.nextLine());
+        Inventory item = Main.garage.get(idx - 1);
+        item.quantity = toUpdate;
 
+    }
 
+    public void printOutInventory() {
+        for (Inventory item : Main.garage) {
+            System.out.printf("%d: %s \n", item.quantity, item.item);
+        }
+        System.out.println("----------------------");
+    }
+
+    public void removeInventory(Scanner scanner) {
+        System.out.println("Enter item number to remove it. ([-1] to cancel)");
+        int numToRemove = Integer.parseInt(scanner.nextLine());
+        if (numToRemove > 0) {
+            Main.garage.remove(numToRemove - 1);
+        }
+
+    }
+
+    public void addItem(Scanner scanner){
+        String item = getItem(scanner);
+        int quantity = getQuantity(scanner);
+        Inventory thing = new Inventory(item, quantity);
+        Main.garage.add(thing);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    public static ArrayList<Inventory> garage = new ArrayList<>();
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Inventory> garage = new ArrayList<>();
+
         while (true) {
             Inventory inventory = new Inventory();
             System.out.println("1.) Add some Inventory\n2.) Remove some inventory\n3.) Update some inventory quantity. \n4.) View Inventory.");
             String userInput = scanner.nextLine();
             if (userInput.equals("1")) {
-                String item = inventory.getItem(scanner);
-                int quantity = inventory.getQuantity(scanner);
-                Inventory thing = new Inventory(item, quantity);
-                garage.add(thing);
+                inventory.addItem(scanner);
 
             } else if (userInput.equals("2")) {
-                System.out.println("Enter item number to remove it. ");
-                int numToRemove = Integer.parseInt(scanner.nextLine());
-                garage.remove(numToRemove - 1);
+                inventory.removeInventory(scanner);
 
             } else if (userInput.equals("3")) {
-                System.out.println("What number would you like to update? ");
-                int idx = Integer.parseInt(scanner.nextLine());
-                System.out.println("What would you like to change to: ");
-                int toUpdate = Integer.parseInt(scanner.nextLine());
-                Inventory item = garage.get(idx - 1);
-                item.quantity = toUpdate;
+                inventory.updateGarage(scanner);
 
             } else if( userInput.equals("4")){
-                for (Inventory item : garage) {
-                    System.out.printf("%d: %s \n", item.quantity, item.item);
-                }
-                System.out.println("----------------------");
+                inventory.printOutInventory();
+
             } else {
                 System.out.println("Not a valid choice.");
             }
